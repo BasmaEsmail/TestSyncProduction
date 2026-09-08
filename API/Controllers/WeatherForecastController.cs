9cc4f5ce14576c20a28b4e97eea65095033d8fa4
BasmaEsmail <basmaabas755@gmail.com>
2026-09-08 12:11:13 +0300

    test

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace API.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("GetColdTemp", Name = "GetColdTemp")]
+        public IEnumerable<WeatherForecast> GetColdTemp()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 0),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
